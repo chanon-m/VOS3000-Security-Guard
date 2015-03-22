7da2cd60836fafb64a5c7288291a3630f64cc693
Chanon <chanon_m@hotmail.com>
2015-03-22 14:02:51 -0700

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <?php
-    include_once("./includes/web.config.inc.php");
+    include_once("./includes/db.config.inc.php");
 
     if($_POST['action'] ==  'Log In') {
         $username   =   trim($_POST['username']);
